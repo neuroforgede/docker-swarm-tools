f0c5c4354019ec652a9119ea5270fa7b76b78b53
Martin Braun <martinbraun123@aol.com>
2022-01-08 17:57:18 +0100

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM nginx:1.21.5-alpine
+
+RUN mkdir -p /etc/nginx/templates
+
+COPY ./nginx.conf /etc/nginx/templates/nginx.conf
+COPY ./run.sh /etc/nginx/run.sh
+
+RUN chmod +x /etc/nginx/run.sh
+
+ENV WORKER_CONNECTIONS=768
+ENV DNS_RESOLVER=127.0.0.11
+
+CMD /etc/nginx/run.sh
---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+check_result () {
+    ___RESULT=$?
+    if [ $___RESULT -ne 0 ]; then
+        echo $1
+        exit 1
+    fi
+}
+
+envsubst '$WORKER_CONNECTIONS, $DNS_RESOLVER, $TCP_UPSTREAM' < /etc/nginx/templates/nginx.conf > /etc/nginx/nginx.conf
+check_result "failed to template /etc/nginx/nginx.conf"
+
+if [ -z "$TCP_UPSTREAM" ]; then
+    echo "env variable TCP_UPSTREAM must be set"
+    exit 1
+fi
+
+exec nginx -g 'daemon off;'
